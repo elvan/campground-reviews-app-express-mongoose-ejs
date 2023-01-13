f3076489921e84802a39116131d0710cf69a1a07
Elvan Hidayat <elvanhd@gmail.com>
2023-01-13 23:15:40 +0700

    creating the basic express app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
