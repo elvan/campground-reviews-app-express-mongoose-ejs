f7b57c306c22db6f434a334c211750a6fd7a368a
Elvan Hidayat <elvanhd@gmail.com>
2023-03-07 06:33:20 +0700

    campgrounds show

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+
 // Connect to the database
 mongoose
   .connect(MONGODB_URI, {
