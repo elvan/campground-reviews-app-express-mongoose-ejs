839d0f07e2e610cc3d349b27ceb2cfe730b6400d
Elvan Hidayat <elvanhd@gmail.com>
2023-03-07 06:50:41 +0700

    campground edit and update

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // Load environment variables
 require('dotenv').config();
 
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/campground-reviews-app';
+
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 
 const Campground = require('./models/campground');
-
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/campground-reviews-app';
 
 // Create an express app
 const app = express();
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // Routes
 app.get('/', (req, res) => {
@@ -42,6 +44,17 @@ app.get('/campgrounds', async (req, res) => {
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 // Connect to the database
