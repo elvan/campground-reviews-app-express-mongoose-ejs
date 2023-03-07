6b0c4cecaa5e9b9f871146829aec397798a87c65
Elvan Hidayat <elvanhd@gmail.com>
2023-03-07 13:11:23 +0700

    creating reviews

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const Campground = require('./models/campground');
+const Review = require('./models/review');
+
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const { campgroundSchema } = require('./schemas.js');
@@ -97,6 +99,18 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+  })
+);
+
+app.post(
+  '/campgrounds/:id/reviews',
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
