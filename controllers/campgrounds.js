1a359d49a74ca0eadd9f660943de9864b93e85ff
Elvan Hidayat <elvanhd@gmail.com>
2023-03-31 23:20:59 +0700

    add geocoding using mapbox

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,10 @@
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+
 const Campground = require('../models/campground');
 const { cloudinary } = require('../cloudinary');
+
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
@@ -11,7 +16,14 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   campground.images = req.files.map((f) => ({ url: f.path, filename: f.filename }));
   campground.author = req.user._id;
   await campground.save();
