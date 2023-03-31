e36347c43c24ff3ad9376c27135f07ab42038efc
Elvan Hidayat <elvanhd@gmail.com>
2023-03-31 14:51:36 +0700

    add an author to campground

--- models/campground.js
+++ models/campground.js
@@ -4,19 +4,28 @@ const Review = require('./review');
 
 const Schema = mongoose.Schema;
 
-const CampgroundSchema = new Schema({
-  title: String,
-  image: String,
-  price: Number,
-  description: String,
-  location: String,
-  reviews: [
-    {
+const CampgroundSchema = new Schema(
+  {
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String,
+    author: {
       type: Schema.Types.ObjectId,
-      ref: 'Review',
+      ref: 'User',
     },
-  ],
-});
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Review',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
   if (doc) {
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -36,6 +36,7 @@ router.post(
   validateCampground,
   catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -45,7 +46,15 @@ router.post(
 router.get(
   '/:id',
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id)
+      .populate({
+        path: 'reviews',
+        populate: {
+          path: 'author',
+        },
+      })
+      .populate('author');
+    console.log(campground);
     if (!campground) {
       req.flash('error', 'Cannot find that campground!');
       return res.redirect('/campgrounds');
