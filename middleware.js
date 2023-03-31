21816184e0c1f71cdb011ed142f170393c4a982e
Elvan Hidayat <elvanhd@gmail.com>
2023-03-31 16:23:31 +0700

    reviews authorization

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -13,6 +14,16 @@ module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user._id)) {
+    req.flash('error', 'You do not have permission to do that!');
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  if (!review.author.equals(req.user._id)) {
     req.flash('error', 'You do not have permission to do that!');
     return res.redirect(`/campgrounds/${id}`);
   }
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const Campground = require('../models/campground');
-const Review = require('../models/review');
-const { reviewSchema } = require('../schemas.js');
-const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
+const Review = require('../models/review');
+const catchAsync = require('../utils/catchAsync');
+const { isReviewAuthor, isLoggedIn } = require('../middleware');
+const { reviewSchema } = require('../schemas.js');
 
 const router = express.Router({ mergeParams: true });
 
@@ -20,10 +21,12 @@ const validateReview = (req, res, next) => {
 
 router.post(
   '/',
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -34,6 +37,8 @@ router.post(
 
 router.delete(
   '/:reviewId',
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
