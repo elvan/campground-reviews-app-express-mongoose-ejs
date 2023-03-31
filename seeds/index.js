e632d70297e80897074ab810a645d309b599c027
Elvan Hidayat <elvanhd@gmail.com>
2023-03-31 14:51:22 +0700

    update seeds data

--- seeds/index.js
+++ seeds/index.js
@@ -24,10 +24,11 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 20; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: '642680bce628cd8378bc0013',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image:
