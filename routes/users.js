54d17d7ab6ef11d81a9f97f6a17727c449cf6be7
Elvan Hidayat <elvanhd@gmail.com>
2023-03-31 14:16:27 +0700

    add a logout handler

--- routes/users.js
+++ routes/users.js
@@ -44,4 +44,14 @@ router.post(
   }
 );
 
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    req.flash('success', 'Goodbye!');
+    res.redirect('/campgrounds');
+  });
+});
+
 module.exports = router;
